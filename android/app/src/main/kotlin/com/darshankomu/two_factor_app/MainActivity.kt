1323b940eece54d6779714e3843390566d1ad49c
darshan <darsh2605@gmail.com>
2024-03-17 23:32:40 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/darshankomu/two_factor_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.darshankomu.two_factor_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_2fa/flutter_2fa.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Scaffold(body: MyHome()));
+  }
+}
+
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+          padding: const EdgeInsets.all(25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                  width: double.infinity,
+                  height: 56,
+                  child: ElevatedButton(
+                    onPressed: () => Flutter2FA().activate(
+                        context: context,
+                        appName: "2 factor authentication app multi factor authentication",
+                        email: "aryangajakosh@gmail.com"),
+                    child: const Text('Activate 2FA'),
+                  )),
+              const SizedBox(height: 30),
+              SizedBox(
+                  width: double.infinity,
+                  height: 56,
+                  child: ElevatedButton(
+                    onPressed: () => Flutter2FA()
+                        .verify(context: context, page: const Success()),
+                    style: ButtonStyle(
+                      backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.green),
+                    ),
+                    child: const Text('Login with 2FA'),
+                  ))
+            ],
+          )),
+    );
+  }
+}
+
+class Success extends StatelessWidget {
+  const Success({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("user logged In Successfully!")),
+    );
+  }
+}
